ea02e5d1789945cceba74e2f7932fd2845c17ec2
Harshal0902 <harshalraikwar07@gmail.com>
2026-02-12 09:43:09 +0530

    ✨ Add buy and sell

--- src/app/_provider/Providers.tsx
+++ src/app/_provider/Providers.tsx
@@ -30,31 +30,3 @@ export default function Providers({
         </WagmiProvider>
     );
 }
-// "use client";
-
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { WagmiProvider } from "wagmi";
-// import {
-//     RainbowKitProvider,
-//     darkTheme,
-// } from "@rainbow-me/rainbowkit";
-// import { config } from "@/lib/wagmi";
-
-// import "@rainbow-me/rainbowkit/styles.css";
-
-// const queryClient = new QueryClient();
-
-// export function Providers({ children }: { children: React.ReactNode }) {
-//     return (
-//         <WagmiProvider config={config}>
-//             <QueryClientProvider client={queryClient}>
-//                 <RainbowKitProvider
-//                     theme={darkTheme()}
-//                     initialChain={8453} // Base chain ID
-//                 >
-//                     {children}
-//                 </RainbowKitProvider>
-//             </QueryClientProvider>
-//         </WagmiProvider>
-//     );
-// }
--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -1,24 +1,24 @@
-// import { createConfig, http } from "wagmi";
-// import { base } from "wagmi/chains";
-// import { injected } from "wagmi/connectors";
-
-// export const config = createConfig({
-//     chains: [base],
-//     connectors: [
-//         injected({
-//             shimDisconnect: true,
-//             target: "metaMask", // ✅ important fallback
-//         }),
-//     ],
-//     transports: {
-//         [base.id]: http("https://mainnet.base.org"),
-//     },
-// });
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { createConfig, http } from "wagmi";
 import { base } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
 
-export const config = getDefaultConfig({
-    appName: "Your App",
-    projectId: "ac0698015835d4ab536cbe42447851f6", // get from walletconnect.com
+export const config = createConfig({
     chains: [base],
+    connectors: [
+        injected({
+            shimDisconnect: true,
+            target: "metaMask", // ✅ important fallback
+        }),
+    ],
+    transports: {
+        [base.id]: http("https://mainnet.base.org"),
+    },
 });
+// import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+// import { base } from "wagmi/chains";
+
+// export const config = getDefaultConfig({
+//     appName: "Your App",
+//     projectId: "ac0698015835d4ab536cbe42447851f6", // get from walletconnect.com
+//     chains: [base],
+// });
